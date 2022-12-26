9925cd41804c2768132aaa73312e82ab6a9ba921
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-27 00:02:43 +0100

    remove not used

--- src/routes/Eigenschaften-Sammlungen/[...].js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import App from '../../components/App'
-
-const EigenschaftenSammlungen = () => <App />
-
-export default EigenschaftenSammlungen
--- src/routes/Lebensräume/[...].js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import App from '../../components/App'
-
-const Lebensräume = () => <App />
-
-export default Lebensräume
