da8677d673cde7bf707a27d0e604673733c177fc
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-02-14 14:35:55 +0100

    clean

--- src/modules/getLastIdFromUrl.js
+++ src/modules/getLastIdFromUrl.js
@@ -4,7 +4,6 @@ const getLastIdFromUrl = (url) => {
   if (!url) return undefined
   if (url.length === 0) return undefined
   const last = url.at(-1)
-  console.log('getLastIdFromUrl, last:', last)
   if (isUuid.anyNonNil(last)) {
     return last
   }
