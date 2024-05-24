c18776dacab839245c3025c72a28dfb70e60ae69
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-24 13:53:46 +0200

    optimize

--- src/modules/setLoginFromIdb.js
+++ src/modules/setLoginFromIdb.js
@@ -1,4 +1,4 @@
-const setLoginFromIdb = async ({ idb, store }) => {
+export const setLoginFromIdb = async ({ idb, store }) => {
   const users = await idb.users.toArray()
   const token = users?.[0]?.token
   const username = users?.[0]?.username
@@ -8,5 +8,3 @@ const setLoginFromIdb = async ({ idb, store }) => {
   }
   return store
 }
-
-export default setLoginFromIdb
