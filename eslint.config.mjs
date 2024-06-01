38e6451795830d91019cdf3c92d31d2a40d19f1d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-01 17:13:25 +0200

    upgrade eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,80 +1,97 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import _import from "eslint-plugin-import";
-import { fixupPluginRules, fixupConfigRules } from "@eslint/compat";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import reactRefresh from 'eslint-plugin-react-refresh'
+import _import from 'eslint-plugin-import'
+import { fixupConfigRules } from '@eslint/compat'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default [{
-    ignores: ["**/dist", "**/node_modules/"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-)), {
+export default [
+  {
+    ignores: ['**/dist', '**/node_modules/'],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/stylistic',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+    ),
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        react: fixupPluginRules(react),
-        "react-hooks": fixupPluginRules(reactHooks),
-        import: fixupPluginRules(_import),
+      'react-refresh': reactRefresh,
+      import: _import,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: 'detect',
+      },
     },
 
     rules: {
-        strict: 0,
-        "react/prop-types": 0,
-        "react/display-name": "off",
-        "no-console": "off",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "prefer-const": "warn",
-        "no-unused-vars": "off",
+      strict: 0,
+      'react/prop-types': 0,
+      'react/display-name': 'off',
+      'no-console': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prefer-const': 'warn',
+      'no-unused-vars': 'off',
 
-        "no-empty": ["error", {
-            allowEmptyCatch: true,
-        }],
+      'no-empty': [
+        'error',
+        {
+          allowEmptyCatch: true,
+        },
+      ],
 
-        "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
 
-        "import/extensions": ["warn", "ignorePackages", {
-            jsx: "never",
-            ts: "never",
-            tsx: "never",
-        }],
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      'import/extensions': [
+        'warn',
+        'ignorePackages',
+        {
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
     },
-}];
+  },
+]
