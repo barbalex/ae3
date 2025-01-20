46adc6f042c0c72e4c5e0a60a1e7767796c16f8a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-20 11:01:51 +0100

    style

--- src/components/Layout/AppBar/MoreMenu.jsx
+++ src/components/Layout/AppBar/MoreMenu.jsx
@@ -26,7 +26,7 @@ const StyledButton = styled(Button)`
 `
 const Version = styled.div`
   padding: 12px 16px;
-  color: rgba(0, 0, 0, 0.4);
+  color: rgba(0, 0, 0, 0.5);
   user-select: none;
 `
 // const TwoLineMenuItem = styled(MenuItem)`
