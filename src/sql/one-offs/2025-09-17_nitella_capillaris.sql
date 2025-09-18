70d218917973b9a9c576807f702368541cafada2
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-18 09:39:47 +0200

    add nitella capillaris

---
+++ src/sql/one-offs/2025-09-17_nitella_capillaris.sql
@@ -0,0 +1,14 @@
+insert into ae.object (id, taxonomy_id, parent_id, name, properties)
+values
+  ('45531c06-ddb3-4c9d-b834-0579631f7862', 'aed47d41-7b0e-11e8-b9a5-bd4f79edbcc4', '6d03d6a0-5244-11e9-a001-3381f3be0680', 'Nitella capillaris (Krock.) J. Groves & Bull.-Webst.', '{
+  "Art": "capillaris",
+  "Status": "eigenständige Art aber im Index nicht enthalten",
+  "Artname": "Nitella capillaris (Krock.) J. Groves & Bull.-Webst.",
+  "Familie": "Characeae",
+  "Gattung": "Nitella",
+  "Bemerkungen": "Nachträglich importiert, 17.09.2025, ag",
+  "InfoFloraUrl": "https://www.infoflora.ch/en/flora/nitella-capillaris.html",
+  "Taxonomie ID": "50032",
+  "Familie früher": "unbestimmt",
+  "Artname vollständig": "Nitella capillaris (Krock.) J. Groves & Bull.-Webst."
+}'::jsonb);
