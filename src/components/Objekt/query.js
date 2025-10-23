9ea522fa05c80c2260f9f5a9ca4007b575893764
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-23 11:54:17 +0200

    export explicits

--- src/components/Objekt/query.js
+++ src/components/Objekt/query.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-export default gql`
+export const query = gql`
   query ObjectQuery($objectId: UUID!) {
     objectById(id: $objectId) {
       id
--- src/components/Objekt/querySynonyms.js
+++ src/components/Objekt/querySynonyms.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-export default gql`
+export const querySynonyms = gql`
   query ObjectQuery($objectIds: [UUID!]) {
     pcs: allPropertyCollections(
       filter: {
