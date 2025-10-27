9f0bbb79bdad017b84d9d44f906d5db86bfb3924
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-27 11:30:21 +0100

    use observer

--- src/components/shared/NewProperty.jsx
+++ src/components/shared/NewProperty.jsx
@@ -1,4 +1,5 @@
 import { useState, useContext } from 'react'
+import { observer } from 'mobx-react-lite'
 import TextField from '@mui/material/TextField'
 import InputLabel from '@mui/material/InputLabel'
 import styled from '@emotion/styled'
@@ -17,61 +18,48 @@ const FieldContainer = styled.div`
   display: flex;
 `
 
-export const NewProperty = ({ id, properties: propertiesPrevious }) => {
-  const apolloClient = useApolloClient()
-  const queryClient = useQueryClient()
+export const NewProperty = observer(
+  ({ id, properties: propertiesPrevious }) => {
+    const apolloClient = useApolloClient()
+    const queryClient = useQueryClient()
 
-  const store = useContext(storeContext)
-  const { scrollIntoView } = store
+    const store = useContext(storeContext)
+    const { scrollIntoView } = store
 
-  const [label, setLabel] = useState('')
-  const [value, setValue] = useState('')
+    const [label, setLabel] = useState('')
+    const [value, setValue] = useState('')
 
-  const onChangeLabel = (event) => setLabel(event.target.value)
-  const onChangeValue = (event) => setValue(event.target.value)
-  const onBlurValue = async (event) => {
-    const { value } = event.target
-    if (value !== null && value !== undefined && !!label) {
-      const properties = {
-        ...propertiesPrevious,
-        ...{ [label]: value },
+    const onChangeLabel = (event) => setLabel(event.target.value)
+    const onChangeValue = (event) => setValue(event.target.value)
+    const onBlurValue = async (event) => {
+      const { value } = event.target
+      if (value !== null && value !== undefined && !!label) {
+        const properties = {
+          ...propertiesPrevious,
+          ...{ [label]: value },
+        }
+        await apolloClient.mutate({
+          mutation: updatePropertyMutation,
+          variables: { properties: JSON.stringify(properties), id },
+        })
+        setLabel('')
+        setValue('')
+        await queryClient.invalidateQueries({
+          queryKey: ['tree'],
+        })
+        scrollIntoView()
       }
-      await apolloClient.mutate({
-        mutation: updatePropertyMutation,
-        variables: { properties: JSON.stringify(properties), id },
-      })
-      setLabel('')
-      setValue('')
-      await queryClient.invalidateQueries({
-        queryKey: ['tree'],
-      })
-      scrollIntoView()
     }
-  }
 
-  return (
-    <ErrorBoundary>
-      <Container>
-        <InputLabel>Neues Feld:</InputLabel>
-        <FieldContainer>
-          <TextField
-            label="Feld-Name"
-            value={label}
-            onChange={onChangeLabel}
-            fullWidth
-            multiline
-            autoComplete="off"
-            autoCorrect="off"
-            autoCapitalize="off"
-            spellCheck="false"
-            variant="standard"
-          />
-          {!!label && (
+    return (
+      <ErrorBoundary>
+        <Container>
+          <InputLabel>Neues Feld:</InputLabel>
+          <FieldContainer>
             <TextField
-              label="Feld-Wert"
-              value={value}
-              onChange={onChangeValue}
-              onBlur={onBlurValue}
+              label="Feld-Name"
+              value={label}
+              onChange={onChangeLabel}
               fullWidth
               multiline
               autoComplete="off"
@@ -80,9 +68,24 @@ export const NewProperty = ({ id, properties: propertiesPrevious }) => {
               spellCheck="false"
               variant="standard"
             />
-          )}
-        </FieldContainer>
-      </Container>
-    </ErrorBoundary>
-  )
-}
+            {!!label && (
+              <TextField
+                label="Feld-Wert"
+                value={value}
+                onChange={onChangeValue}
+                onBlur={onBlurValue}
+                fullWidth
+                multiline
+                autoComplete="off"
+                autoCorrect="off"
+                autoCapitalize="off"
+                spellCheck="false"
+                variant="standard"
+              />
+            )}
+          </FieldContainer>
+        </Container>
+      </ErrorBoundary>
+    )
+  },
+)
