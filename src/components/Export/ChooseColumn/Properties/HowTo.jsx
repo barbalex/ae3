ca95f1a86acb98543abf5e01f5f078e0f420c24e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-11 14:36:18 +0100

    use css module

--- src/components/Export/ChooseColumn/Properties/HowTo.jsx
+++ src/components/Export/ChooseColumn/Properties/HowTo.jsx
@@ -17,9 +17,6 @@ const StyledCardActions = styled(CardActions)`
   cursor: pointer;
   height: auto !important;
 `
-const CardActionIconButton = styled(IconButton)`
-  transform: ${(props) => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
-`
 const CardActionTitle = styled.div`
   padding-left: 8px;
   font-weight: bold;
@@ -43,15 +40,15 @@ export const HowTo = () => {
         onClick={() => setExpanded(!expanded)}
       >
         <CardActionTitle>So geht's</CardActionTitle>
-        <CardActionIconButton
-          data-expanded={expanded}
+        <IconButton
           aria-expanded={expanded}
           aria-label="Show more"
+          style={{ transform: expanded ? 'rotate(180deg)' : 'none' }}
         >
           <Icon>
             <ExpandMoreIcon />
           </Icon>
-        </CardActionIconButton>
+        </IconButton>
       </StyledCardActions>
       <Collapse
         in={expanded}
