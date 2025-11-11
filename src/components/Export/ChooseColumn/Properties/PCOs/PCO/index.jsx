4e551efd6b79b7819f447085812e8be353a912ec
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-11 15:17:16 +0100

    use css module

--- src/components/Export/ChooseColumn/Properties/PCOs/PCO/index.jsx
+++ src/components/Export/ChooseColumn/Properties/PCOs/PCO/index.jsx
@@ -21,9 +21,6 @@ const StyledCardActions = styled(CardActions)`
   background-color: #fff3e0;
   border-bottom: 1px solid #ebebeb;
 `
-const CardActionIconButton = styled(IconButton)`
-  transform: ${(props) => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
-`
 const CardActionTitle = styled.div`
   padding-left: 8px;
   font-weight: bold;
@@ -52,15 +49,15 @@ export const PCO = ({ pcName, count }) => {
             {pcName}
             <Count>{`(${count} ${count === 1 ? 'Feld' : 'Felder'})`}</Count>
           </CardActionTitle>
-          <CardActionIconButton
-            data-expanded={expanded}
+          <IconButton
             aria-expanded={expanded}
             aria-label="Show more"
+            style={{ transform: expanded ? 'rotate(180deg)' : 'none' }}
           >
             <Icon>
               <ExpandMoreIcon />
             </Icon>
-          </CardActionIconButton>
+          </IconButton>
         </StyledCardActions>
         <StyledCollapse
           in={expanded}
