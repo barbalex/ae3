76f5d1288941fef1435ab480d0c35fc583f1484b
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-11 16:02:53 +0100

    use css module

--- src/components/Export/ChooseColumn/Properties/RCOs/RcList/RCO/index.jsx
+++ src/components/Export/ChooseColumn/Properties/RCOs/RcList/RCO/index.jsx
@@ -34,9 +34,6 @@ const StyledCardActions = styled(CardActions)`
   height: auto !important;
   display: flex;
 `
-const CardActionIconButton = styled(IconButton)`
-  transform: ${(props) => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
-`
 const CardActionTitle = styled.div`
   padding-left: 8px;
   font-weight: bold;
@@ -123,15 +120,15 @@ export const RCO = observer(({ pcname, relationtype, count }) => {
               count === 1 ? 'Feld' : 'Felder'
             })`}</Count>
           </CardActionTitle>
-          <CardActionIconButton
-            data-expanded={expanded}
+          <IconButton
             aria-expanded={expanded}
             aria-label="Show more"
+            style={{ transform: expanded ? 'rotate(180deg)' : 'none' }}
           >
             <Icon>
               <ExpandMoreIcon />
             </Icon>
-          </CardActionIconButton>
+          </IconButton>
         </StyledCardActions>
         <StyledCollapse
           in={expanded}
