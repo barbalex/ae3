3c12cf4cf0b45b51238561ce85df027825489ba4
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-13 11:58:57 +0100

    use css module

--- src/components/Tree/LoadingRow.jsx
+++ src/components/Tree/LoadingRow.jsx
@@ -1,11 +1,10 @@
 import styled from '@emotion/styled'
 import CircularProgress from '@mui/material/CircularProgress'
 
-const singleRowHeight = 23
 const StyledNode = styled.div`
   padding-left: ${(props) => `${Number(props['data-level']) * 17 - 5}px`};
-  height: ${singleRowHeight}px;
-  max-height: ${singleRowHeight}px;
+  height: 23px;
+  max-height: 23px;
   box-sizing: border-box;
   margin: 0;
   margin-bottom: 4px;
