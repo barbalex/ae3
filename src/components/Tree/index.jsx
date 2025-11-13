43ba4157c70f975969b1b5adc53f80766ca35dcf
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-13 11:57:54 +0100

    use css module

--- src/components/Tree/index.jsx
+++ src/components/Tree/index.jsx
@@ -1,5 +1,4 @@
 import { useMemo, useContext } from 'react'
-import styled from '@emotion/styled'
 import { useApolloClient } from '@apollo/client/react'
 import { observer } from 'mobx-react-lite'
 import SimpleBar from 'simplebar-react'
@@ -20,99 +19,7 @@ import { constants } from '../../modules/constants.js'
 import { Root } from './Root/index.jsx'
 import { IntoViewScroller } from './IntoViewScroller.jsx'
 
-const ErrorContainer = styled.div`
-  padding: 24px;
-`
-const Container = styled.div`
-  height: 100%;
-  /*display: flex;
-  flex-direction: column;
-  overflow: hidden;*/
-  ul {
-    margin: 0;
-    list-style: none;
-    padding: 0 0 0 1.1em;
-  }
-
-  /*
-   * context menu
-   */
-
-  .react-contextmenu {
-    display: flex;
-    flex-direction: column;
-    min-width: 100px;
-    padding: 5px 0;
-    margin: 2px 0 0;
-    font-size: 14px;
-    text-align: left;
-    background-color: rgb(66, 66, 66);
-    background-clip: padding-box;
-    border: 1px solid grey;
-    border-radius: 0.25rem;
-    outline: none;
-    opacity: 0;
-    pointer-events: none;
-    font-family: 'Roboto', sans-serif;
-  }
-
-  .react-contextmenu.react-contextmenu--visible {
-    color: white;
-    opacity: 1;
-    pointer-events: auto;
-    z-index: 1000;
-  }
-
-  .react-contextmenu-title {
-    opacity: 0;
-  }
-
-  .react-contextmenu--visible .react-contextmenu-title {
-    color: #b3b3b3;
-    padding-left: 10px;
-    padding-right: 15px;
-    padding-bottom: 3px;
-    opacity: 1;
-  }
-
-  .react-contextmenu > .react-contextmenu-item {
-    display: inline-block;
-    padding: 3px 20px;
-    clear: both;
-    font-weight: 400;
-    line-height: 1.5;
-    color: white;
-    text-align: inherit;
-    white-space: nowrap;
-    background: 0 0;
-    border: 0;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  .react-contextmenu-item.active,
-  .react-contextmenu-item:hover {
-    color: #f57c00;
-    border-color: #0275d8;
-    text-decoration: none;
-  }
-  .react-contextmenu-divider {
-    border-top: 1px solid grey;
-    margin-top: 4px;
-    margin-bottom: 7px;
-  }
-  .react-contextmenu-submenu {
-    padding-right: 27px !important;
-  }
-
-  .react-contextmenu-submenu:after {
-    content: '▶';
-    display: inline-block;
-    position: absolute;
-    right: 7px;
-    bottom: 3px;
-  }
-`
+import { errorContainer } from './index.module.css'
 
 const getUserIsTaxWriter = (data) => {
   const userRoles = (
@@ -144,13 +51,15 @@ export const Tree = observer(() => {
 
   if (error) {
     return (
-      <ErrorContainer>{`Error fetching data: ${error.message}`}</ErrorContainer>
+      <div
+        className={errorContainer}
+      >{`Error fetching data: ${error.message}`}</div>
     )
   }
 
   return (
     <ErrorBoundary>
-      <Container>
+      <div className="tree-container">
         <Filter />
         <SimpleBar
           style={{
@@ -174,7 +83,7 @@ export const Tree = observer(() => {
             <CmPC />
           </>
         )}
-      </Container>
+      </div>
     </ErrorBoundary>
   )
 })
