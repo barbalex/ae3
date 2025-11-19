44ad7e3876119c9a53c0bdf7214c04112edd5f30
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-19 14:51:54 +0100

    clean

--- src/components/Export/ChooseColumn/Properties/RCOs/RcList/RCO/index.jsx
+++ src/components/Export/ChooseColumn/Properties/RCOs/RcList/RCO/index.jsx
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import Collapse from '@mui/material/Collapse'
 import IconButton from '@mui/material/IconButton'
-import Icon from '@mui/material/Icon'
 import { MdExpandMore as ExpandMoreIcon } from 'react-icons/md'
 import { gql } from '@apollo/client'
 import { useQuery } from '@apollo/client/react'
@@ -101,9 +100,7 @@ export const RCO = observer(({ pcname, relationtype, count }) => {
             aria-label="Show more"
             style={{ transform: expanded ? 'rotate(180deg)' : 'none' }}
           >
-            <Icon>
-              <ExpandMoreIcon />
-            </Icon>
+            <ExpandMoreIcon />
           </IconButton>
         </CardActions>
         <Collapse
