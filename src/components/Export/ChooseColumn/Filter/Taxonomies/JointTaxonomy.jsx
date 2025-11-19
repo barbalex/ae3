61e982d850edee815933dd36bd6f7cb0426db35d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-19 14:49:17 +0100

    clean

--- src/components/Export/ChooseColumn/Filter/Taxonomies/JointTaxonomy.jsx
+++ src/components/Export/ChooseColumn/Filter/Taxonomies/JointTaxonomy.jsx
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import Collapse from '@mui/material/Collapse'
 import IconButton from '@mui/material/IconButton'
-import Icon from '@mui/material/Icon'
 import { MdExpandMore as ExpandMoreIcon } from 'react-icons/md'
 
 import { Properties } from './Properties.jsx'
@@ -38,9 +37,7 @@ export const JointTaxonomy = ({ jointTaxProperties }) => {
             aria-label="Show more"
             style={{ transform: expanded ? 'rotate(180deg)' : 'none' }}
           >
-            <Icon>
-              <ExpandMoreIcon />
-            </Icon>
+            <ExpandMoreIcon />
           </IconButton>
         </CardActions>
         <Collapse
