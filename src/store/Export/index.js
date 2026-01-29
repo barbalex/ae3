e231b8d07fb04a0ecdfa61b3a049186a16979683
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-29 01:02:12 +0100

    migrate to jotai

--- src/store/Export/index.js
+++ src/store/Export/index.js
@@ -1,18 +1,5 @@
 import { types } from 'mobx-state-tree'
 
-import { constants } from '../../modules/constants.js'
-import {
-  jotaiStore,
-  exportTaxPropertiesAtom,
-  exportPcoPropertiesAtom,
-  exportRcoPropertiesAtom,
-  exportTaxFiltersAtom,
-  exportPcoFiltersAtom,
-  exportRcoFiltersAtom,
-  exportWithSynonymDataAtom,
-  exportTooManyPropertiesAtom,
-} from '../../jotaiStore/index.ts'
-
 export default types
   .model('Export', {
     addFilterFields: types.optional(types.boolean, true),
