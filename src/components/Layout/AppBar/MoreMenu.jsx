428d729d63631c248c6e5237674df1b8decd3ffa
Alexander Gabriel <alex.barbalex@gmail.com>
2026-02-24 16:30:38 +0100

    up version

--- src/components/Layout/AppBar/MoreMenu.jsx
+++ src/components/Layout/AppBar/MoreMenu.jsx
@@ -55,9 +55,9 @@ export const MoreMenu = () => {
 
     const hostname = window.location.hostname
     const uptimeUrl =
-      hostname === 'localhost'
-        ? 'https://uptime.artdaten.ch'
-        : `//uptime.${hostname.replace('www.', '')}`
+      hostname === 'localhost' ?
+        'https://uptime.artdaten.ch'
+      : `//uptime.${hostname.replace('www.', '')}`
     window.open(uptimeUrl)
     setAnchorEl(null)
   }
@@ -84,19 +84,28 @@ export const MoreMenu = () => {
         onClose={onClose}
         PaperProps={paperProps}
       >
-        <MenuItem key="ueber" onClick={onClickUeber}>
+        <MenuItem
+          key="ueber"
+          onClick={onClickUeber}
+        >
           über arteigenschaften.ch
         </MenuItem>
-        <MenuItem key="relations" onClick={onClickStruktur}>
+        <MenuItem
+          key="relations"
+          onClick={onClickStruktur}
+        >
           Daten-Struktur
         </MenuItem>
-        <MenuItem key="melden" onClick={onClickMelden}>
+        <MenuItem
+          key="melden"
+          onClick={onClickMelden}
+        >
           Fehler oder Wünsche melden
         </MenuItem>
         <MenuItem onClick={onClickUptime}>
           {`Verfügbarkeit der Server von ${hostname}`}
         </MenuItem>
-        <div className={styles.version}>Version: 3.1.54 vom 23.2.2026</div>
+        <div className={styles.version}>Version: 3.1.55 vom 24.2.2026</div>
       </Menu>
     </div>
   )
