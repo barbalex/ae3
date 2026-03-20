fd25d0cfa33b1536bf7d754268a4e90f80f14ca4
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-20 12:19:43 +0100

    catch build problem

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import babel from '@rolldown/plugin-babel'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 5174 },
+  build: {
+    minify: 'esbuild',
+  },
   plugins: [
     svgrPlugin({
       svgrOptions: {
