3333d5001c213cb68e8040af453f1e25b9c9ba43
Alexander Gabriel <alex.barbalex@gmail.com>
2026-05-04 22:35:35 +0200

    up version

--- src/components/Layout/AppBar/MoreMenu.jsx
+++ src/components/Layout/AppBar/MoreMenu.jsx
@@ -107,7 +107,7 @@ export const MoreMenu = () => {
         <MenuItem onClick={onClickUptime}>
           {`Verfügbarkeit der Server von ${hostname}`}
         </MenuItem>
-        <div className={styles.version}>Version: 3.1.63 vom 4.5.2026</div>
+        <div className={styles.version}>Version: 3.1.64 vom 4.5.2026</div>
       </Menu>
     </div>
   )
